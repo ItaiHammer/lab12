1eacd952a8e122ecbe7747495f25a7ed03e73fab
Itai Hammer <61294759+ItaiHammer@users.noreply.github.com>
2023-11-17 09:50:14 -0800

    Added 2

--- eclipse-workspace/lab12/src/trees/FamilyTree.java
+++ eclipse-workspace/lab12/src/trees/FamilyTree.java
@@ -33,22 +33,24 @@ public class FamilyTree
         {
             // Add childNode to this node's children list. Also
             // set childNode's parent to this node.
+        	children.add(childNode);
+        	childNode.parent = this;
         }
         
         
         // Searches subtree at this node for a node
         // with the given name. Returns the node, or null if not found.
-        TreeNode getNodeWithName(String targetName)
-        {
-            // Does this node have the target name?
-            if (?????)
+        TreeNode getNodeWithName(String targetName) {
+            if (this.name.equals(targetName))
                 return this;
                     
             // No, recurse. Check all children of this node.
-            for (TreeNode child: children)
+            for (TreeNode child : children)
             {
-                // If child.getNodeWithName(targetName) returns a non-null node,
-                // then that's the node we're looking for. Return it.
+                TreeNode result = child.getNodeWithName(targetName);
+                if (result != null) {
+                	return result;
+                }
             }
             
             // Not found anywhere.
@@ -61,12 +63,17 @@ public class FamilyTree
         ArrayList<TreeNode> collectAncestorsToList()
         {
             ArrayList<TreeNode> ancestors = new ArrayList<>();
+            TreeNode current = this;
 
             // ?????  Collect ancestors of this TreeNode into the array list. HINT: going up
             // the nodes of a tree is like traversing a linked list. If that isn’t clear,
             // draw a tree, mark any leaf node, and then mark its ancestors in order from
             // recent to ancient. Expect a question about this on the final exam.
-
+            while (current.parent != null) {
+            	ancestors.add(current.parent);
+            	current = current.parent;
+            }
+            
             return ancestors;
         }
         
@@ -109,11 +116,14 @@ public class FamilyTree
 
 		// Parse the input file. Create a FileReader that reads treeFile. Create a BufferedReader
 		// that reads from the FileReader.
-		FileReader fr = ???
-		BufferedReader br = ???
+		FileReader fr = new FileReader(treeFile);
+		BufferedReader br = new BufferedReader(fr);
 		String line;
-		while ((line = br.readLine()) != null)
+		
+		while ((line = br.readLine()) != null) {
 			addLine(line);
+		}
+		
 		br.close();
 		fr.close();
 	}
